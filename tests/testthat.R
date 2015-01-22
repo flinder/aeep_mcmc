acb243279131b691b59bb979367f66508a275b25
Fridolin Linder <fridolin.linder@gmail.com>
2015-01-21 22:24:35 -0500

    Adds testing functionality

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(irtpar)
+
+test_check("irtpar")
--- tests/testthat/test_linmod.R
+++ tests/testthat/test_linmod.R
@@ -8,16 +8,17 @@ y <- x1 - x2 + rnorm(N)
 dat <- data.frame(y, x1, x2) 
 
 # Example function: linear regression
-fit <- function(data){
+fit <- function(dat){
   require(rjags)
-  N <- nrow(data)
-  dataList <- list(x = cbind(data$x1, data$x2), y = data$y, N = N, K = 2)
+  N <- nrow(dat)
+  dataList <- list(x = cbind(dat$x1, dat$x2), y = dat$y, N = N, K = 2)
   initsList <- list(b0 = -1, b = c(0, 0), tau = 1)
   params = c("b0", "b", "tau")
-  jmod <- jags.model("linear_regression.txt", data = dataList, n.chains = 1, n.adapt = 500)
+  jmod <- jags.model("tests/testthat/linear_regression.txt", data = dataList, 
+                     n.chains = 1, n.adapt = 500)
   mcmcres <- coda.samples(model = jmod, variable.names = params, 
                           n.iter = 1000, thin = 1)
   return(mcmcres[[1]])
 }
-
+fit(dat)
 out <- parallel_mcmc(data = dat, cores = 4, combine = "parametric")
